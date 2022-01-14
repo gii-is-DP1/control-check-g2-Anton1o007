23e5232863afcf8288efccf4e4dd154e57ac84aa
Antonio_MO <antmirote@us.es>
2022-01-14 12:28:11 +0100

    entrega antmirote

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
@@ -1,5 +1,13 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.ManyToAny;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -7,9 +15,16 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class FeedingType {
-    Integer id;
+public class FeedingType extends BaseEntity{
+
+    @NotEmpty
+    @Size(min=3,max=50)
+    @Column(unique = true)
     String name;
+    @NotEmpty
     String description;
+    @ManyToOne
+    @NotNull
+    @Column(name="pet_type_id")
     PetType petType;
 }
